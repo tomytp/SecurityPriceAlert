2a8e024ba6b7da64e2d09ae6d671a9687a3b3176
tomytp <thomazrebellomiranda@gmail.com>
2024-06-23 08:46:48 -0300

    feat: Argument parsing and validation

--- src/Program.cs
+++ src/Program.cs
@@ -8,16 +8,47 @@ class Program
     static async Task Main(string[] args)
     {
         var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-
-        string securitySymbol = "PETR4";
-        double sellTriggerPrice = 40.0;
-        double buyTriggerPrice = 38.0;
         
         AppSettings? settings = config.GetRequiredSection("AppSettings").Get<AppSettings>();
-        if (settings == null) return;
+        if (settings == null) 
+        {
+            Console.WriteLine("Invalid Configuration file! Check appsettings.json.");
+            return;
+        }
+        
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Usage: StockQuoteAlert <securitySymbol> <sellTriggerPrice> <buyTriggerPrice>");
+            return;
+        }
+        
+        string securitySymbol = args[0];
+        if (!double.TryParse(args[1], out double sellTriggerPrice))
+        {
+            Console.WriteLine("Invalid Sell Trigger. Please provide a valid number.");
+            return;
+        }
+
+        if (!double.TryParse(args[2], out double buyTriggerPrice))
+        {
+            Console.WriteLine("Invalid Buy Trigger. Please provide a valid number.");
+            return;
+        }
 
         SecurityMonitor securityMonitor = new SecurityMonitor(settings.BrApiToken);
         MailHandler mailHandler = new MailHandler(settings.SmtpCredentials);
+        
+        bool? isValidSymbol = await securityMonitor.IsValidSymbol(securitySymbol);
+        if (isValidSymbol == null)
+        {
+            Console.WriteLine("Could not validate symbol!");
+            return;
+        } 
+        if ((bool)!isValidSymbol)
+        {
+            Console.WriteLine("Security symbol is invalid!");
+            return;
+        }
         
         securityMonitor.SellTrigger += async (_, quote) =>
         {
