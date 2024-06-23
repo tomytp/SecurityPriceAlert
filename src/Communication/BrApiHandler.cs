105d2cc87f9d6ab19765f4c79a6e8231aad259c1
tomytp <thomazrebellomiranda@gmail.com>
2024-06-22 21:12:00 -0300

    feat: Basic ApiHandler

---
+++ src/Communication/BrApiHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace SecurityPriceAlert.Communication;
+
+public class BrApiHandler
+{
+    private readonly HttpClient _httpClient;
+
+    public BrApiHandler()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://brapi.dev/api/")
+        };
+    }
+    
+    public async Task<BrApiSearch?> GetSecuritiesBySymbol(string securitySymbol, string token)
+    {
+        return await _httpClient.GetFromJsonAsync<BrApiSearch>($"available?search={securitySymbol}&token={token}");
+    }
+}
+
+public record BrApiSearch(
+    List<string> indexes,
+    List<string> stocks
+);  
--- src/Program.cs
+++ src/Program.cs
@@ -9,9 +9,19 @@ class Program
     {
         var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-        SmtpCredentials? credentials = config.GetRequiredSection("SmtpCredentials").Get<SmtpCredentials>();
-        if (credentials == null) return;
-        MailHandler mailHandler = new MailHandler(credentials);
-        await mailHandler.SendMailAsync("inoamailalert@gmail.com", "email test", "body.......");
+        AppSettings? settings = config.GetRequiredSection("AppSettings").Get<AppSettings>();
+        if (settings == null) return;
+        BrApiHandler brApiHandler = new BrApiHandler();
+        // MailHandler mailHandler = new MailHandler(settings.SmtpCredentials);
+        // await mailHandler.SendMailAsync(settings.DestinationEmailAddress, "email test", "body.......");
+        (await brApiHandler.GetSecuritiesBySymbol("PETR4", settings.BrApiToken))?.stocks.ForEach(Console.WriteLine);
+        Console.WriteLine("--------");
+        (await brApiHandler.GetSecuritiesBySymbol("PETRS4", settings.BrApiToken))?.stocks.ForEach(Console.WriteLine);
     }
 }
+
+public record AppSettings(
+    SmtpCredentials SmtpCredentials,
+    string DestinationEmailAddress,
+    string BrApiToken
+);
